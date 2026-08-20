a8efd163a96f02b8d1fd2d1c1b4f3f702f4fcf04
Le-Hung Tran <2512033252@student.hcmus.edu.vn>
2026-08-20 21:08:16 +0700

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # remaining = (')', ']', '}')
+        oBracket = ['(', '[', '{']
+        cBracket = [')', ']', '}']
+        remaining = [0, 0, 0]
+
+        stack = []
+        last = -1
+
+        for c in s:
+            if c in oBracket:
+                stack.append(c)
+                remaining[oBracket.index(c)] += 1
+                last = oBracket.index(c)
+
+            if c in cBracket:
+                if not stack or remaining[cBracket.index(c)] <= 0 or cBracket.index(c) != last:
+                    return False
+                
+                remaining[cBracket.index(c)] -= 1
+                stack.pop()
+                last = oBracket.index(stack[-1]) if stack else -1
+
+        if stack:
+            return False
+        
+        return True
